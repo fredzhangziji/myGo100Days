a3d6163af300bc4a14ce5f3c50c90f3445bfd993
Fred <fredzhangziji@gmail.com>
2021-09-24 17:08:45 -0400

    day 1 golang

---
+++ Day1/src/hello/helloworld.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt" // formatting IO
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// Ignore the package name when we call the method in the package
+// import(
+// 	. "fmt"
+// )
+
+// func main() {
+// 	Println("Hello, World!")
+// }
+
+// Replace the package name when we call the method in the package
+// import (
+// 	f "fmt"
+// 	)
+// func main() {
+// 	f.Println("Hello, World!")
+// }
---
+++ Day1/src/hello/helloworld2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" // formatting IO
+
+func main() {
+	fmt.Println("I am the second helloworld file.")
+	fmt.Print("Go Go Go!!!!!!!!")
+}
